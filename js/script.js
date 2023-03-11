8e64e0a97d6acd08090c497888621ae8ac95e7b6
TSB <122724396+tsbproject@users.noreply.github.com>
2023-03-11 17:22:08 +0100

    [committed]

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-const d = new Date();
-document.getElementById("demo").innerHTML = d;
+let h =  "Monday - Satuday 9am - 6pm"
+   
+    
+    document.getElementById("workhour").innerHTML = h;
+
+
+// Create a new <i> element with classes of "bi" and "bi-clock"
+const icon = document.createElement("i");
+icon.classList.add("bi", "bi-clock");
+
+// Get the <div> element with an id of "icon"
+const iconContainer = document.getElementById("clock");
+
+// Add the icon element to the container
+iconContainer.appendChild(clock);
+
+
