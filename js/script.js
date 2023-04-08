06accff0567937dfd486b980d997883f9edb1c43
TSB <122724396+tsbproject@users.noreply.github.com>
2023-04-08 23:59:25 +0100

    [committed]

--- js/script.js
+++ js/script.js
@@ -65,21 +65,16 @@ dropdowns.forEach((dropdown) => {
 
 
  // PORTFOLIO CAROUSEL
- let items = document.querySelectorAll('.carousel .carousel-item')
+ $('.multicarousel').on('slide.bs.carousel', function (event) {
+  var $next = $(event.relatedTarget);
+  var $target = $('.carousel-inner', event.target);
+  var $width = $next.width();
 
- items.forEach((el) => {
-   const minPerSlide = 4
-   let next = el.nextElementSibling
-   for (var i=1; i<minPerSlide; i++) {
-     if (!next) {
-         // wrap carousel by using first child
-         next = items[0]
-     }
-     let cloneChild = next.cloneNode(true)
-     el.appendChild(cloneChild.children[0])
-     next = next.nextElementSibling
- }
-})
+  if (!$target.is(':animated')) {
+    $target.animate({scrollLeft: $next.position().left}, 500);
+  }
+});
+
 
 
 
