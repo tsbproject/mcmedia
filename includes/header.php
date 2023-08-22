f460f962b7fab539b2fbccc368df22868cd03f8c
TSB <122724396+tsbproject@users.noreply.github.com>
2023-08-22 09:13:00 +0100

    [committed]

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,49 @@
-<!--------------------HEADER SECTION START------------------------------>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Marvel Creative Media</title>
+    <link rel="icon" type="image/x-icon" href="/images/marvelcmedia-favicon.png">
+     
+     <!--------------------Bootstrap CSS---------------------->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+     rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+     
+   
+   <!-------------------JS Library---------------------------------->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+ integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
-<header class=" header fixed-top">
+  
+   <!---------JQuary Library-------------->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>  
+     
+     <!----------------------------------Custom CSS------------------------------->
+  <link rel="stylesheet" href="css/styles.css">
+
+ 
+  <!----------------Boostrap Icons--------------->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  
+  <!--------Google Fonts------------------->
+  <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+
+  <!------------------- FONT AWESONE ICONS CDN----------------------------------->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity=
+   "sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+  
+  
+  
+  </head>
+ 
+  
+  <body>
+    
+  <!--------------------HEADER SECTION START------------------------------>
+
+    <header class="fixed-top">
       <div class="header-info">
         <div class="container d-none d-md-flex justify-content-evenly py-1">
            <i id="Telephone" class="bi bi-telephone-fill">&nbsp;&nbsp;<span id="number">(+234) 818-601-9049</span> </i> 
@@ -112,7 +155,7 @@
               <li class="nav-item">
                   <a class="nav-link" href="request-a-quote.html">Request a Quote</a></li> 
               <li class="nav-item">
-                  <a class="nav-link"  href="contact.html">Contact us</a></li>    
+                  <a class="nav-link" href="contact.html">Contact us</a></li>    
                   
                   
             </ul>
@@ -120,3 +163,5 @@
           </div>
       </nav>
     </header>
+
+     <!--------------------HEADER SECTION END------------------------------>
